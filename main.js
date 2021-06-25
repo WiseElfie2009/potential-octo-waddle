bc1c3cab97f036cfa7b4361b6cfba0d4af5d6f24
WiseElfie2009 <83706514+WiseElfie2009@users.noreply.github.com>
2021-06-25 17:53:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function setup(){
     canvas.center();
     video=createCapture(VIDEO);
     video.hide();
-    classifier=ml5.imageClasssifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json'),modelLoaded;
+    classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json'),modelLoaded;
 }
 
 function modelLoaded(){
